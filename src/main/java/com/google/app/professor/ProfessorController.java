c0c4be2192a5a921fd6537366b0c2732b2a37201
minsushin-kor <minsushin425@gmail.com>
2026-04-09 10:43:41 +0900

    update / delete / create 추가

--- src/main/java/com/google/app/professor/ProfessorController.java
+++ src/main/java/com/google/app/professor/ProfessorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.google.app.departments.DepartmentDTO;
@@ -29,5 +30,35 @@ public class ProfessorController {
 		professorDTO = professorService.detail(professorDTO);
 		
 		model.addAttribute("d", professorDTO);
+	}
+		
+	@GetMapping("update")
+	public void update(ProfessorDTO professorDTO, Model model) throws Exception {
+		professorDTO = professorService.detail(professorDTO);
+		model.addAttribute("d", professorDTO);
+	}
+	
+	@PostMapping("update")
+	public String update(ProfessorDTO professorDTO) throws Exception{
+		
+		int result = professorService.update(professorDTO);
+		return "redirect:./list";
+	}
+	
+	@GetMapping("create")
+	public void create() {}
+	
+	@PostMapping("create")
+	public String create(ProfessorDTO professorDTO) throws Exception {
+		
+		int result = professorService.create(professorDTO);		
+		return "redirect:./list";
+	}
+	
+	@PostMapping("delete")
+	public String delete(ProfessorDTO professorDTO) throws Exception {
+		
+		int result = professorService.delete(professorDTO);
+		return "redirect:./list";
 	}
 }
--- src/main/java/com/google/app/professor/ProfessorService.java
+++ src/main/java/com/google/app/professor/ProfessorService.java
@@ -19,4 +19,15 @@ public class ProfessorService {
 		return professorMapper.detail(professorDTO);
 	}
 	
+	public int update(ProfessorDTO professorDTO) throws Exception{
+		return professorMapper.update(professorDTO);
+	}
+	
+	public int create(ProfessorDTO professorDTO) throws Exception{
+		return professorMapper.create(professorDTO);
+	}
+	
+	public int delete(ProfessorDTO professorDTO) throws Exception{
+		return professorMapper.delete(professorDTO);
+	}
 }
